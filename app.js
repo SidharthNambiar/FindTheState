9a82c43ca01aff446940c35bb0b42c7c15d9971d
Sidharth Nambiar <snambiar01@gmail.com>
2022-05-13 22:13:29 -0400

    added cheat and hint options

--- app.js
+++ app.js
@@ -1,110 +1,175 @@
-const countrySelect = document.querySelector("#countrySelect");
-const countries = document.querySelectorAll(".countries");
-const stateLabel = document.querySelector("#state");
+const mapSelect = document.querySelector("#mapSelect");
+const maps = document.querySelectorAll(".maps");
+const locationLabel = document.querySelector("#location");
 const gameStatus = document.querySelector("#status");
-const reset = document.querySelector("button");
-let allStates = [];
-let statesToSelect = [];
-let clickedState = null;
-let chosenStateIdx = null;
-let stateToSelect = null;
+const reset = document.querySelector("#reset");
+const hint = document.querySelector("#hint");
+const cheat = document.querySelector("#cheat");
+
+let allLocations = [];
+let clickedLocation = null;
+let chosenLocationIdx = null;
+let locationToSelect = null;
 let count = null;
-let clickedStates = [];
-let countrySelected = "usa";
-let map = document.querySelectorAll(`#${countrySelected} path`);
+let clickedLocations = [];
+let mapSelected = "usa";
+let toggle = 1;
+let map = document.querySelectorAll(`#${mapSelected} path`);
 gameStatus.style.display = "none";
 
 reset.disabled = true;
+hint.disabled = true;
+cheat.disabled = true;
 
-countrySelect.addEventListener("change", (e) => {
-  reset.disabled = false;
-  stateLabel.textContent = "";
-  countrySelected = countrySelect.value;
-  countrySelect.disabled = true;
-
-  for (let country of countries) {
-    if (country.id !== countrySelected) {
-      country.hidden = true;
-    } else {
-      country.hidden = false;
+hint.addEventListener("click", () => {
+  for (let location of map) {
+    if (locationToSelect === location.dataset.name) {
+      if (toggle) {
+        location.style.fill = "#ffe08a";
+        gameStatus.style.display = "";
+         gameStatus.classList.value = "notification is-warning";
+          gameStatus.textContent = "Hint Revealed!";
+        toggle = 0;
+      } else {
+        gameStatus.style.display = "none"
+        location.style.fill = "white";
+        toggle = 1;
+      }
     }
   }
-  map = document.querySelectorAll(`#${countrySelected} path`);
-  allStates = [];
-  for (let state of map) {
-    allStates.push(state.dataset.name);
-    state.style.fill = "white";
+});
+
+mapSelect.addEventListener("change", (e) => {
+  reset.disabled = false;
+  hint.disabled = false;
+  cheat.disabled = false;
+  locationLabel.textContent = "";
+  mapSelected = mapSelect.value;
+  mapSelect.disabled = true;
+
+  //Only display the selected map; hide the others
+  for (let map of maps) {
+    if (map.id !== mapSelected) {
+      map.hidden = true;
+    } else {
+      map.hidden = false;
+    }
   }
-  console.log(allStates.length);
-  statesToSelect = allStates.slice();
-  clickedState = null;
-  chosenStateIdx = Math.floor(Math.random() * allStates.length - 1);
-  stateToSelect = allStates[chosenStateIdx];
-  stateLabel.textContent = stateToSelect.toUpperCase();
+
+  // Given the large size of the world, in order to display it completely, bulma is-flex is used
+  if (mapSelected === "world") {
+    const div = document.querySelector("#world");
+    div.classList.add("is-flex");
+  }
+
+  // querying all the paths(locations) within the selected map
+  map = document.querySelectorAll(`#${mapSelected} path`);
+
+  // extracting the names of all the locations and putting them in a an array called allLocations
+  allLocations = [];
+  for (let location of map) {
+    allLocations.push(location.dataset.name);
+    location.style.fill = "white";
+  }
+
+  clickedLocation = null;
+  chosenLocationIdx = Math.floor(Math.random() * allLocations.length - 1);
+  locationToSelect = allLocations[chosenLocationIdx];
+
+  locationLabel.textContent = locationToSelect.toUpperCase();
   gameStatus.style.display = "none";
 
-  stateLabel.style.color = "dark-grey";
-  count = allStates.length;
+  locationLabel.style.color = "dark-grey";
+  count = allLocations.length;
 
-  for (let state of map) {
-    allStates.push(state.dataset.name);
-    state.style.fill = "white";
-  }
+  for (let location of map) {
+    location.addEventListener("click", (e) => {
+      clickedLocation = location.dataset.name;
 
-  for (let state of map) {
-    state.addEventListener("click", (e) => {
-      clickedState = state.dataset.name;
-
-      if (clickedState === stateToSelect) {
+      if (clickedLocation === locationToSelect) {
         gameStatus.style.display = "none";
-
-        for (let state of clickedStates) {
-          state.style.fill = "white";
+        if (clickedLocations.length !== 0) {
+          for (let location of clickedLocations) {
+            location.style.fill = "white";
+          }
+          clickedLocations = [];
         }
-        clickedStates = [];
-        state.style.fill = "mediumseagreen";
+        location.style.fill = "mediumseagreen";
         count = count - 1;
         if (count === 0) {
-          stateLabel.textContent = "";
+          locationLabel.textContent = "";
           gameStatus.style.display = "";
 
           gameStatus.classList.value = "notification is-success";
           gameStatus.textContent = "Great Job! You found all locations!";
         } else {
-          statesToSelect.splice(chosenStateIdx, 1);
-          chosenStateIdx = Math.floor(Math.random() * count);
-          stateToSelect = statesToSelect[chosenStateIdx];
-          stateLabel.textContent = stateToSelect.toUpperCase();
+          allLocations.splice(chosenLocationIdx, 1);
+          chosenLocationIdx = Math.floor(Math.random() * count);
+          locationToSelect = allLocations[chosenLocationIdx];
+          locationLabel.textContent = locationToSelect.toUpperCase();
         }
       } else {
-        if (state.style.fill !== "mediumseagreen") {
-          state.style.fill = "#ffe08a";
+        if (location.style.fill !== "mediumseagreen") {
+          location.style.fill = "#ffe08a";
           gameStatus.style.display = "";
-
+          gameStatus.classList.value = "notification is-warning";
           gameStatus.textContent = "Incorrect! Try Again.";
-          clickedStates.push(state);
+          clickedLocations.push(location);
         }
       }
     });
   }
 });
 
-reset.addEventListener("click", (e) => {
-  statesToSelect = allStates.slice();
-  clickedState = null;
-  clickedStates = [];
+cheat.addEventListener("click", (e) => {
+  hint.disabled = true;
+  cheat.disabled = true;
 
-  stateLabel.textContent = "";
+  while (count !== 0) {
+    for (let location of map) {
+      if (location.dataset.name === locationToSelect) {
+        location.style.fill = "mediumseagreen";
+      }
+    }
+    count = count - 1;
+    allLocations.splice(allLocations.indexOf(locationToSelect), 1);
+    chosenLocationIdx = Math.floor(Math.random() * count);
+    locationToSelect = allLocations[chosenLocationIdx];
+    if(count !== 0) locationLabel.textContent = locationToSelect.toUpperCase();
+
+  }
+
+ 
+ 
+ 
+    locationLabel.textContent = "";
+    gameStatus.style.display = "";
+    gameStatus.classList.value = "notification is-success";
+    gameStatus.textContent = "Great Job Cheating! You found all locations!";
+  
+});
+
+reset.addEventListener("click", (e) => {
+  allLocations = allLocations.slice();
+  clickedLocation = null;
+  clickedLocations = [];
+
+  locationLabel.textContent = "";
   gameStatus.textContent = "";
   gameStatus.style.display = "none";
 
-  for (let state of map) {
-    state.style.fill = "white";
+  for (let location of map) {
+    location.style.fill = "white";
   }
-  countrySelect.disabled = false;
-  for (let country of countries) {
-    country.hidden = true;
+  mapSelect.disabled = false;
+  for (let map of maps) {
+    map.hidden = true;
   }
   reset.disabled = true;
-  countrySelect.value = "";
+  mapSelect.value = "";
+
+  if (mapSelected === "world") {
+    const div = document.querySelector("#world");
+    div.classList.remove("is-flex");
+  }
 });
