a96bcbb6c32336d045afc961d85ff5af58dcf8c2
Sidharth Nambiar <snambiar01@gmail.com>
2022-05-14 19:43:23 -0400

    added hint beacon

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const gameStatus = document.querySelector("#status");
 const reset = document.querySelector("#reset");
 const hint = document.querySelector("#hint");
 const cheat = document.querySelector("#cheat");
+const body = document.querySelector("body");
+const hintBeacon = document.createElement("span");
 
 let allLocations = [];
 let clickedLocation = null;
@@ -15,6 +17,8 @@ let clickedLocations = [];
 let mapSelected = "usa";
 let toggle = 1;
 let map = document.querySelectorAll(`#${mapSelected} path`);
+let hintLocation = null;
+
 gameStatus.style.display = "none";
 
 reset.disabled = true;
@@ -25,13 +29,34 @@ hint.addEventListener("click", () => {
   for (let location of map) {
     if (locationToSelect === location.dataset.name) {
       if (toggle) {
-        location.style.fill = "#ffe08a";
+        console.log(location.getBoundingClientRect());
+        hintLocation = location.getBoundingClientRect();
+        hintBeacon.style.left = hintLocation.left + "px";
+        hintBeacon.style.right = hintLocation.right + "px";
+        hintBeacon.style.top = hintLocation.top + "px";
+        hintBeacon.style.bottom = hintLocation.bottom + "px";
+        hintBeacon.style.x = hintLocation.x + "px";
+        hintBeacon.style.y = hintLocation.y + "px";
+
+        // beacon.style.position = "absolute"
+        body.append(hintBeacon);
+        hintBeacon.classList.add("beacon");
+        // location.style.fill = "#ffe08a";
         gameStatus.style.display = "";
-         gameStatus.classList.value = "notification is-warning";
-          gameStatus.textContent = "Hint Revealed!";
+        gameStatus.classList.value = "notification is-link";
+        gameStatus.textContent = "Hint Revealed!";
         toggle = 0;
       } else {
-        gameStatus.style.display = "none"
+        hintBeacon.classList.remove("beacon")
+        hintLocation = ""
+        hintBeacon.style.left = ""
+        hintBeacon.style.right = ""
+        hintBeacon.style.top = ""
+        hintBeacon.style.bottom = ""
+        hintBeacon.style.x = ""
+        hintBeacon.style.y = ""
+        
+        gameStatus.style.display = "none";
         location.style.fill = "white";
         toggle = 1;
       }
@@ -84,9 +109,25 @@ mapSelect.addEventListener("change", (e) => {
 
   for (let location of map) {
     location.addEventListener("click", (e) => {
+      let x = e.x;
+      let y = e.y;
+      console.log(x, y);
+      console.dir(location);
+
+      // console.dir(beacon)
+
       clickedLocation = location.dataset.name;
 
       if (clickedLocation === locationToSelect) {
+        hintBeacon.classList.remove("beacon")
+        hintLocation = ""
+        hintBeacon.style.left = ""
+        hintBeacon.style.right = ""
+        hintBeacon.style.top = ""
+        hintBeacon.style.bottom = ""
+        hintBeacon.style.x = ""
+        hintBeacon.style.y = ""
+        toggle = 1;
         gameStatus.style.display = "none";
         if (clickedLocations.length !== 0) {
           for (let location of clickedLocations) {
@@ -121,32 +162,75 @@ mapSelect.addEventListener("change", (e) => {
   }
 });
 
-cheat.addEventListener("click", (e) => {
-  hint.disabled = true;
-  cheat.disabled = true;
-
-  while (count !== 0) {
-    for (let location of map) {
-      if (location.dataset.name === locationToSelect) {
-        location.style.fill = "mediumseagreen";
-      }
-    }
-    count = count - 1;
-    allLocations.splice(allLocations.indexOf(locationToSelect), 1);
-    chosenLocationIdx = Math.floor(Math.random() * count);
-    locationToSelect = allLocations[chosenLocationIdx];
-    if(count !== 0) locationLabel.textContent = locationToSelect.toUpperCase();
-
+let keyCount = 0;
+body.addEventListener("keyup", (e) => {
+  if (e.key === "h") {
+    keyCount++;
+  } else {
+    keyCount = 0;
   }
 
- 
- 
- 
+  if (keyCount === 5) {
+    keyCount = 0;
+    hint.disabled = true;
+    cheat.disabled = true;
+
+    while (count !== 0) {
+      for (let location of map) {
+        if (location.dataset.name === locationToSelect) {
+          location.style.fill = "mediumseagreen";
+        }
+      }
+      count = count - 1;
+      allLocations.splice(allLocations.indexOf(locationToSelect), 1);
+      chosenLocationIdx = Math.floor(Math.random() * count);
+      locationToSelect = allLocations[chosenLocationIdx];
+      if (count !== 0)
+        locationLabel.textContent = locationToSelect.toUpperCase();
+    }
+
     locationLabel.textContent = "";
     gameStatus.style.display = "";
     gameStatus.classList.value = "notification is-success";
     gameStatus.textContent = "Great Job Cheating! You found all locations!";
-  
+  }
+});
+
+cheat.addEventListener("click", (e) => {
+  count = count - 1;
+  for (let location of map) {
+    
+    if (location.dataset.name === locationToSelect) {
+      location.style.fill = "mediumseagreen";
+
+    }
+  }
+
+  if (count === 0) {
+    locationLabel.textContent = "";
+    gameStatus.style.display = "";
+    gameStatus.classList.value = "notification is-success";
+    gameStatus.textContent = "Great Job Cheating! You found all locations!";
+  } else {
+    allLocations.splice(allLocations.indexOf(locationToSelect), 1);
+    chosenLocationIdx = Math.floor(Math.random() * count);
+    locationToSelect = allLocations[chosenLocationIdx];
+    if (count !== 0) {
+      locationLabel.textContent = locationToSelect.toUpperCase();
+    }
+  }
+
+  if (!toggle) {
+    hintBeacon.classList.remove("beacon")
+    hintLocation = ""
+    hintBeacon.style.left = ""
+    hintBeacon.style.right = ""
+    hintBeacon.style.top = ""
+    hintBeacon.style.bottom = ""
+    hintBeacon.style.x = ""
+    hintBeacon.style.y = ""
+    toggle = 1;
+  }
 });
 
 reset.addEventListener("click", (e) => {
@@ -166,10 +250,23 @@ reset.addEventListener("click", (e) => {
     map.hidden = true;
   }
   reset.disabled = true;
+  hint.disabled = true;
+  cheat.disabled = true;
   mapSelect.value = "";
 
   if (mapSelected === "world") {
     const div = document.querySelector("#world");
     div.classList.remove("is-flex");
   }
+  hintBeacon.classList.remove("beacon")
+        hintLocation = ""
+        hintBeacon.style.left = ""
+        hintBeacon.style.right = ""
+        hintBeacon.style.top = ""
+        hintBeacon.style.bottom = ""
+        hintBeacon.style.x = ""
+        hintBeacon.style.y = ""
+        
+  
+  hintBeacon.remove();
 });
