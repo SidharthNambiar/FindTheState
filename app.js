29ef343b6a3e80d5400847f66778715f3a369dc2
Sidharth Nambiar <snambiar01@gmail.com>
2022-05-15 17:17:12 -0400

    added countdown timer for beacon

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const hint = document.querySelector("#hint");
 const cheat = document.querySelector("#cheat");
 const body = document.querySelector("body");
 const hintBeacon = document.createElement("span");
+const modal = document.querySelector(".modal");
 
 let allLocations = [];
 let clickedLocation = null;
@@ -24,42 +25,61 @@ gameStatus.style.display = "none";
 reset.disabled = true;
 hint.disabled = true;
 cheat.disabled = true;
+let timerCnt = 5;
+let timerIntervalId = null;
+let timeoutId = null
 
-hint.addEventListener("click", () => {
+// Get ms left to timeout
+
+hint.addEventListener("click", (e) => {
+  hint.disabled = true;
+
   for (let location of map) {
     if (locationToSelect === location.dataset.name) {
-      if (toggle) {
-        console.log(location.getBoundingClientRect());
-        hintLocation = location.getBoundingClientRect();
-        hintBeacon.style.left = hintLocation.left + "px";
-        hintBeacon.style.right = hintLocation.right + "px";
-        hintBeacon.style.top = hintLocation.top + "px";
-        hintBeacon.style.bottom = hintLocation.bottom + "px";
-        hintBeacon.style.x = hintLocation.x + "px";
-        hintBeacon.style.y = hintLocation.y + "px";
+      hintLocation = location.getBoundingClientRect();
 
-        // beacon.style.position = "absolute"
-        body.append(hintBeacon);
-        hintBeacon.classList.add("beacon");
-        // location.style.fill = "#ffe08a";
+      hintBeacon.style.left = hintLocation.left + "px";
+      hintBeacon.style.right = hintLocation.right + "px";
+      hintBeacon.style.top = hintLocation.top + "px";
+      hintBeacon.style.bottom = hintLocation.bottom + "px";
+      hintBeacon.style.x = hintLocation.x + "px";
+      hintBeacon.style.y = hintLocation.y + "px";
+      hintBeacon.classList.add("beacon");
+
+      // location.style.fill = "#ffe08a";
+      gameStatus.style.display = "";
+      gameStatus.classList.value = "notification is-link";
+      gameStatus.textContent = `HINT COUNTDOWN: ${timerCnt}`;
+      timerCnt--;
+      toggle = 0;
+
+      body.append(hintBeacon);
+
+      timeoutId = setTimeout(() => {
+        hint.disabled = false;
+        hintBeacon.classList.remove("beacon");
+        hintLocation = "";
+        hintBeacon.style.left = "";
+        hintBeacon.style.right = "";
+        hintBeacon.style.top = "";
+        hintBeacon.style.bottom = "";
+        hintBeacon.style.x = "";
+        hintBeacon.style.y = "";
+
+        gameStatus.style.display = "none";
+        clearInterval(timerIntervalId);
+        timerCnt = 5;
+        toggle = 1;
+      }, 5000);
+
+     
+
+      timerIntervalId = setInterval(() => {
         gameStatus.style.display = "";
         gameStatus.classList.value = "notification is-link";
-        gameStatus.textContent = "Hint Revealed!";
-        toggle = 0;
-      } else {
-        hintBeacon.classList.remove("beacon")
-        hintLocation = ""
-        hintBeacon.style.left = ""
-        hintBeacon.style.right = ""
-        hintBeacon.style.top = ""
-        hintBeacon.style.bottom = ""
-        hintBeacon.style.x = ""
-        hintBeacon.style.y = ""
-        
-        gameStatus.style.display = "none";
-        location.style.fill = "white";
-        toggle = 1;
-      }
+        gameStatus.textContent = `HINT COUNTDOWN: ${timerCnt}`;
+        timerCnt--;
+      }, 1000);
     }
   }
 });
@@ -95,6 +115,8 @@ mapSelect.addEventListener("change", (e) => {
   for (let location of map) {
     allLocations.push(location.dataset.name);
     location.style.fill = "white";
+    location.classList.add("js-modal-trigger");
+    // location.setAttribute("data-target","modal-js-example")
   }
 
   clickedLocation = null;
@@ -102,6 +124,8 @@ mapSelect.addEventListener("change", (e) => {
   locationToSelect = allLocations[chosenLocationIdx];
 
   locationLabel.textContent = locationToSelect.toUpperCase();
+  hint.textContent = "LOCATE " + locationToSelect.toUpperCase();
+
   gameStatus.style.display = "none";
 
   locationLabel.style.color = "dark-grey";
@@ -109,52 +133,60 @@ mapSelect.addEventListener("change", (e) => {
 
   for (let location of map) {
     location.addEventListener("click", (e) => {
-      let x = e.x;
-      let y = e.y;
-      console.log(x, y);
-      console.dir(location);
-
-      // console.dir(beacon)
+      e.stopPropagation();
 
       clickedLocation = location.dataset.name;
 
       if (clickedLocation === locationToSelect) {
-        hintBeacon.classList.remove("beacon")
-        hintLocation = ""
-        hintBeacon.style.left = ""
-        hintBeacon.style.right = ""
-        hintBeacon.style.top = ""
-        hintBeacon.style.bottom = ""
-        hintBeacon.style.x = ""
-        hintBeacon.style.y = ""
+        hintBeacon.classList.remove("beacon");
+        clearInterval(timerIntervalId)
+        clearTimeout(timeoutId)
+        hint.disabled = false;
+        timerCnt = 5;
+        hintLocation = "";
+        hintBeacon.style.left = "";
+        hintBeacon.style.right = "";
+        hintBeacon.style.top = "";
+        hintBeacon.style.bottom = "";
+        hintBeacon.style.x = "";
+        hintBeacon.style.y = "";
         toggle = 1;
         gameStatus.style.display = "none";
         if (clickedLocations.length !== 0) {
           for (let location of clickedLocations) {
-            location.style.fill = "white";
+            if (location.style.fill === "rgb(255, 224, 138)") {
+              location.style.fill = "white";
+            }
           }
           clickedLocations = [];
         }
         location.style.fill = "mediumseagreen";
         count = count - 1;
-        if (count === 0) {
-          locationLabel.textContent = "";
-          gameStatus.style.display = "";
 
-          gameStatus.classList.value = "notification is-success";
-          gameStatus.textContent = "Great Job! You found all locations!";
+        if (count === 0) {
+          modal.classList.add("is-active");
+          // hint.disabled && cheat.disabled;
+          hint.disabled = true;
+          cheat.disabled = true;
+          locationLabel.textContent = "";
+          gameStatus.style.display = "none";
+
+          // gameStatus.classList.value = "notification is-success";
+          // gameStatus.textContent =
+          //   "Great Job! You found all locations! Click on RESET to select another map.";
         } else {
           allLocations.splice(chosenLocationIdx, 1);
           chosenLocationIdx = Math.floor(Math.random() * count);
           locationToSelect = allLocations[chosenLocationIdx];
           locationLabel.textContent = locationToSelect.toUpperCase();
+          hint.textContent = "LOCATE " + locationToSelect.toUpperCase();
         }
       } else {
         if (location.style.fill !== "mediumseagreen") {
           location.style.fill = "#ffe08a";
           gameStatus.style.display = "";
           gameStatus.classList.value = "notification is-warning";
-          gameStatus.textContent = "Incorrect! Try Again.";
+          gameStatus.textContent = `Incorrect! You selected ${location.dataset.name.toUpperCase()}. Try Again`;
           clickedLocations.push(location);
         }
       }
@@ -164,16 +196,24 @@ mapSelect.addEventListener("change", (e) => {
 
 let keyCount = 0;
 body.addEventListener("keyup", (e) => {
-  if (e.key === "h") {
+  if (
+    e.key === "h" &&
+    !Object.values(modal.classList).includes("is-active") &&
+    !cheat.disabled &&
+    !hint.disabled
+  ) {
     keyCount++;
   } else {
     keyCount = 0;
   }
 
-  if (keyCount === 5) {
+  if (keyCount === 5 && !hint.disabled) {
     keyCount = 0;
     hint.disabled = true;
     cheat.disabled = true;
+    clearInterval(timerIntervalId);
+    // clearTimeout(timeoutId)
+    timerCnt = 5;
 
     while (count !== 0) {
       for (let location of map) {
@@ -187,48 +227,69 @@ body.addEventListener("keyup", (e) => {
       locationToSelect = allLocations[chosenLocationIdx];
       if (count !== 0)
         locationLabel.textContent = locationToSelect.toUpperCase();
+      // hint.textContent = "LOCATE " + locationToSelect.toUpperCase();
     }
+    modal.classList.add("is-active");
 
     locationLabel.textContent = "";
-    gameStatus.style.display = "";
-    gameStatus.classList.value = "notification is-success";
-    gameStatus.textContent = "Great Job Cheating! You found all locations!";
+    gameStatus.style.display = "none";
+    // gameStatus.classList.value = "notification is-success";
+    // gameStatus.textContent = "Great Job! You found all locations!";
+  }
+});
+
+body.addEventListener("click", (e) => {
+  if (Object.values(modal.classList).includes("is-active")) {
+    modal.classList.remove("is-active");
   }
 });
 
 cheat.addEventListener("click", (e) => {
+  e.stopPropagation();
+  gameStatus.style.display = "none";
+  clearInterval(timerIntervalId)
+  clearTimeout(timeoutId)
+  hint.disabled = false;
+  timerCnt = 5;
+
   count = count - 1;
   for (let location of map) {
-    
     if (location.dataset.name === locationToSelect) {
       location.style.fill = "mediumseagreen";
-
+    }
+    if (location.style.fill === "rgb(255, 224, 138)") {
+      location.style.fill = "white";
     }
   }
 
   if (count === 0) {
+    cheat.disabled = true;
+    hint.disabled = true;
     locationLabel.textContent = "";
-    gameStatus.style.display = "";
-    gameStatus.classList.value = "notification is-success";
-    gameStatus.textContent = "Great Job Cheating! You found all locations!";
+    gameStatus.style.display = "none";
+    modal.classList.add("is-active");
+    // gameStatus.classList.value = "notification is-success";
+    // gameStatus.textContent = "Great Job Cheating! You found all locations!";
+    
   } else {
     allLocations.splice(allLocations.indexOf(locationToSelect), 1);
     chosenLocationIdx = Math.floor(Math.random() * count);
     locationToSelect = allLocations[chosenLocationIdx];
     if (count !== 0) {
       locationLabel.textContent = locationToSelect.toUpperCase();
+      hint.textContent = "LOCATE " + locationToSelect.toUpperCase();
     }
   }
 
   if (!toggle) {
-    hintBeacon.classList.remove("beacon")
-    hintLocation = ""
-    hintBeacon.style.left = ""
-    hintBeacon.style.right = ""
-    hintBeacon.style.top = ""
-    hintBeacon.style.bottom = ""
-    hintBeacon.style.x = ""
-    hintBeacon.style.y = ""
+    hintBeacon.classList.remove("beacon");
+    hintLocation = "";
+    hintBeacon.style.left = "";
+    hintBeacon.style.right = "";
+    hintBeacon.style.top = "";
+    hintBeacon.style.bottom = "";
+    hintBeacon.style.x = "";
+    hintBeacon.style.y = "";
     toggle = 1;
   }
 });
@@ -258,15 +319,14 @@ reset.addEventListener("click", (e) => {
     const div = document.querySelector("#world");
     div.classList.remove("is-flex");
   }
-  hintBeacon.classList.remove("beacon")
-        hintLocation = ""
-        hintBeacon.style.left = ""
-        hintBeacon.style.right = ""
-        hintBeacon.style.top = ""
-        hintBeacon.style.bottom = ""
-        hintBeacon.style.x = ""
-        hintBeacon.style.y = ""
-        
-  
+  hintBeacon.classList.remove("beacon");
+  hintLocation = "";
+  hintBeacon.style.left = "";
+  hintBeacon.style.right = "";
+  hintBeacon.style.top = "";
+  hintBeacon.style.bottom = "";
+  hintBeacon.style.x = "";
+  hintBeacon.style.y = "";
+
   hintBeacon.remove();
 });
